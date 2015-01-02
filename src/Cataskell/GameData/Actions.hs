725ddb5db452447d636a278f98107ca4d4c7860a
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2015-01-02 00:39:56 -0500

    add discard action

--- src/Cataskell/GameData/Actions.hs
+++ src/Cataskell/GameData/Actions.hs
@@ -8,15 +8,35 @@ import Cataskell.GameData.Player
 
 import GHC.Generics (Generic)
 
-data TradeAction = TradeAction
-  { given :: ResourceCount
-  , received :: ResourceCount
+data TradeOffer = TradeOffer 
+  { offering :: ResourceCount
+  , asking :: ResourceCount
   } deriving (Eq, Show, Ord, Generic)
 
-data Action = Build Construct | Trade TradeAction | Discard ResourceCount
+data TradeAction
+  = Offer TradeOffer
+  | Accept { offer :: TradeOffer  -- ^ the offer in question
+           , asker :: Player      -- ^ the original player who asked to trade
+           }
+  | Reject { offer :: TradeOffer     -- ^ the offer in question
+           , asker :: Player         -- ^ the original player who asked to trade
+           , reason :: Maybe String  -- ^ the optional reason for rejecting the trade
+           }
+  | CompleteTrade { offer :: TradeOffer  -- ^ the offer to be completed
+                  , accepter :: Player   -- ^ the player who accepted the trade of the original offerer
+                  }
+  | CancelTrade { offer :: TradeOffer }  -- ^ the offer being canceled
+  deriving (Eq, Show, Ord, Generic)
+
+data DiscardAction = DiscardAction
+  { amountToDiscard :: Int
+  , resourcesDiscarding :: ResourceCount
+  } deriving (Eq, Show, Ord, Generic)
+
+data Action = Build Construct | Trade TradeAction | Discard DiscardAction
   deriving (Eq, Show, Ord, Generic)
 
 data PlayerAction = PlayerAction
-  { player :: Player
+  { actor :: Player
   , action :: Action
   } deriving (Eq, Show, Ord, Generic)
--- test/Cataskell/GameData/ActionsSpec.hs
+++ test/Cataskell/GameData/ActionsSpec.hs
@@ -5,8 +5,12 @@ import Test.QuickCheck
 
 import Cataskell.GameData.Actions
 import Cataskell.GameData.Player
+import Cataskell.GameData.Resources
 
 instance Arbitrary PlayerAction where
+  arbitrary = undefined
+
+instance Arbitrary DiscardAction where
   arbitrary = undefined
 
 main :: IO ()
@@ -15,5 +19,36 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "A PlayerAction" $ do
-    it "should have an associated player" $ property $
-      \pAction -> validPlayer $ player (pAction :: PlayerAction)
+    it "should have an associated actor" $ do
+      pending
+    it "should have an action" $ do
+      pending
+  describe "A TradeAction" $ do
+    describe "A TradeOffer" $ do
+      it "should have an offering and asking amount" $ do
+        pending
+      it "should be checked against the resources of the player" $ do
+        pending
+    describe "An Accept" $ do
+      it "should contain the original offer and asker" $ do
+        pending
+      it "should be checked against the resources of the player" $ do
+        pending
+    describe "A Reject" $ do
+      it "should contain the original offer and asker" $ do
+        pending
+      it "may contain a reason for rejection" $ do
+        pending
+    describe "A CompleteTrade" $ do
+      it "should be checked against the resources of both players" $ do
+        pending
+      it "should execute the change of resources" $ do
+        pending
+    describe "A CancelTrade" $ do
+      it "should clear all extant trade offers from this player" $ do
+        pending
+  describe "A Discard action" $ do
+    it "should have a necessary amount to discard" $ do
+      pending
+    it "should contain a set of resources that equal the discard amount" $ property $
+      \x -> amountToDiscard (x :: DiscardAction) == (totalResources $ resourcesDiscarding x)
