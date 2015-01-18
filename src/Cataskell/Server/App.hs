e296550e2e2d908182bd3b7dc745955c1c544bf0
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2015-01-18 01:32:30 -0500

    chat

--- src/Cataskell/Server/App.hs
+++ src/Cataskell/Server/App.hs
@@ -5,55 +5,14 @@ module Cataskell.Server.App (server, ServerState (..)) where
 import Prelude hiding (mapM_)
 
 import Control.Monad.IO.Class (liftIO)
-import Data.Aeson ((.=))
 import Data.Foldable (mapM_)
 
 import Control.Applicative
+import Cataskell.Server.MessageTypes
 import qualified Control.Concurrent.STM as STM
 import qualified Data.Aeson as Aeson
 import qualified Data.Text as Text
 import qualified Network.SocketIO as SocketIO
-
-
-data AddUser = AddUser Text.Text
-
-instance Aeson.FromJSON AddUser where
-  parseJSON = Aeson.withText "AddUser" $ pure . AddUser
-
-data NumConnected = NumConnected !Int
-
-instance Aeson.ToJSON NumConnected where
-  toJSON (NumConnected n) = Aeson.object [ "numUsers" .= n]
-
-
-data NewMessage = NewMessage Text.Text
-
-instance Aeson.FromJSON NewMessage where
-  parseJSON = Aeson.withText "NewMessage" $ pure . NewMessage
-
-
-data Said = Said Text.Text Text.Text
-
-instance Aeson.ToJSON Said where
-  toJSON (Said username message) = Aeson.object
-    [ "username" .= username
-    , "message" .= message
-    ]
-
-data UserName = UserName Text.Text
-
-instance Aeson.ToJSON UserName where
-  toJSON (UserName un) = Aeson.object [ "username" .= un ]
-
-
-data UserJoined = UserJoined Text.Text Int
-
-instance Aeson.ToJSON UserJoined where
-  toJSON (UserJoined un n) = Aeson.object
-    [ "username" .= un
-    , "numUsers" .= n
-    ]
-
 
 --------------------------------------------------------------------------------
 data ServerState = ServerState { ssNConnected :: STM.TVar Int }
