bd5652fe4da32c70e5188a05bcb70f4e352152c0
Chris Johnson-Roberson <chris.johnson.roberson@gmail.com>
2015-01-24 00:13:23 -0500

    snap in place

--- src/Cataskell/Server/Main.hs
+++ src/Cataskell/Server/Main.hs
@@ -12,8 +12,11 @@ import qualified Snap.Http.Server as Snap
 import qualified Control.Concurrent.STM as STM
 import qualified Network.SocketIO as SocketIO
 import qualified Network.EngineIO.Snap as EIOSnap
+import Data.ByteString (ByteString)
 import Data.Maybe (maybe)
 import Data.Monoid (mempty)
+import Control.Monad (liftM)
+import Control.Monad.IO.Class (liftIO)
 import System.Environment (lookupEnv)
 
 import Paths_cataskell (getDataDir)
@@ -24,26 +27,36 @@ data App = App
 
 makeLenses ''App
 
-routes = do
+socketIoHandler :: IO (Snap.Handler App App ())
+socketIoHandler = do
   state <- startingState
-  socketIoHandler <- SocketIO.initialize EIOSnap.snapAPI (server state)
-  dataDir <- getDataDir
-  return [ ("/socket.io", socketIoHandler)
-         , ("/", Snap.serveDirectory dataDir)
+  SocketIO.initialize EIOSnap.snapAPI (server state)
+
+staticFiles :: Snap.Handler App App ()
+staticFiles = liftIO getDataDir >>= Snap.serveDirectory
+
+routes :: IO [(ByteString, Snap.Handler App App ())]
+routes = do
+  handler <- socketIoHandler
+  return [ ("/socket.io", handler)
+         , ("/", staticFiles)
          ]
 
-app :: IO (Snap.SnapletInit App App)
+app :: Snap.SnapletInit App App
 app = Snap.makeSnaplet "app" "Cataskell websocket server" (Just getDataDir) $ do
-  s <- startingState
-  Snap.addRoutes routes
+  s <- liftIO startingState
+  routes' <- liftIO routes
+  Snap.addRoutes routes'
   return $ App s
 
+config :: IO (Snap.Config m a)
 config = do
   portNo <- lookupEnv "PORT"
   return $ Snap.setPort (maybe 8080 read portNo) mempty
 
 serverMain :: IO ()
 serverMain = do
-  config' <- Snap.commandLineConfig config
-  Snap.serveSnaplet config app
+  config' <- liftIO config
+  config'' <- Snap.commandLineConfig config'
+  Snap.serveSnaplet config'' app
 
--- test-server/Cataskell/Server/MainSpec.hs
+++ test-server/Cataskell/Server/MainSpec.hs
@@ -4,19 +4,19 @@ module Cataskell.Server.MainSpec (main, spec) where
 import Test.Hspec
 import Test.Hspec.Snap
 import Snap (route)
-import Cataskell.Server.Main (app, routes, startingState)
+import Cataskell.Server.Main (app, staticFiles)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = snap (route routes) app $ do
-  describe "GET /" $
-    it "should serve an HTML page" $
-      get "/" >>= should200
-  describe "GET /main.js" $
-    it "should return JavaScript" $
-      get "/main.js" >>= should200
-  describe "GET /style.css" $
-    it "should return CSS" $
-      get "/style.css" >>= should200
+spec = snap (route [("/", staticFiles)]) app $ do
+    describe "GET /" $
+      it "should serve an HTML page" $
+        get "/" >>= should200
+    describe "GET /main.js" $
+      it "should return JavaScript" $
+        get "/main.js" >>= should200
+    describe "GET /style.css" $
+      it "should return CSS" $
+        get "/style.css" >>= should200
